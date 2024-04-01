d582389fb6f13c3e32db15599b6aef701c702c3a
Joe <37753646+AligatorJoe@users.noreply.github.com>
2024-04-01 11:15:24 +0100

    Add files via upload

---
+++ img.js
@@ -0,0 +1,17 @@
+
+    Serracao_icon = '<img id="Serracao" title="lumber" src="https://i.ibb.co/XCZ7SjZ/Serracao.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Academie_icon = '<img id="Academia" title="Academy" src="https://i.ibb.co/QryCtmn/Academia.png.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Gruta_icon = '<img id="Gruta" title="Cave" src="https://i.ibb.co/PthprsT/Gruta.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Muralha_icon = '<img id="Muralha" title="Wall" src="https://i.ibb.co/DYMnYwB/Muralha.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Senado_icon = '<img id="Senado" title="Senate" src="https://i.ibb.co/hRzW1bz/Senado.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Quartelo_icon = '<img id="Quartelo" title="Quarter" src="https://i.ibb.co/6YXqQ3P/Quartelo.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Porto_icon = '<img id="Porto" title="Harbor" src="https://i.ibb.co/VL7F4Xq/Porto.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Agora_icon = '<img id="Agora" title="Place" src="https://i.ibb.co/ZxGspHS/Agora.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Templo_icon = '<img id="Templo" title="Temple" src="https://i.ibb.co/x58jhDf/Templo.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Mercado_icon = '<img id="Mercado" title="Market" src="https://i.ibb.co/TPMJ7YR/Mercado.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Mina_icon = '<img id="Prata" title="Silver" src="https://i.ibb.co/z717GQW/Mina-Prata.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Pedreira_icon = '<img id="Pedeira" title="Stroner" src="https://i.ibb.co/M96RKrj/Pedreira.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Armazem_icon = '<img id="Armazem" title="Storage" src="https://i.ibb.co/mhjzvwC/Armazim.png" style=" style="margin-top: -14px;  float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Quinta_icon = '<img id="Quinta" title="Farm" src="https://i.ibb.co/hgXjwJs/Quinta.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Simulator_icon = '<img id="Simulador" title="Simulator" src="https://i.ibb.co/9TBGwR7/Simulator.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
+    Exterior_icon = '<img id="Exterior" title="troops abroad" src="https://i.ibb.co/0cP3DT6/Exterior.png" style="margin-top: -14px; float: left; mask: linear-gradient(360deg, #000 75%, transparent 50%);">';
